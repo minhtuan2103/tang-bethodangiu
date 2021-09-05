710277fe23730f081d26905b455384d2904e0c43
minhtuan2103 <90124988+minhtuan2103@users.noreply.github.com>
2021-09-05 12:39:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,15 @@
 const CONFIG = {
     introTitle: 'THÙY à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    introDesc: `Gửi cô bé Vũng Tàu đáng eooo`,
+    btnIntro: 'sến quá cha nội ơi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Tại anh đáng iu quáa <33',
     btnNo: 'Anh bị làm sao ýyyyy :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply:'Yêu thì yêu mà không yêu thì cũng phải yêu nhennnn <3',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Nhắn tin cho anh nhá cô bé đáng iuuuu.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100014013221888' //link mess của các bạn. VD: https://m.me/nam.nodemy
