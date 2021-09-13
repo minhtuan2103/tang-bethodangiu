af9310e51a181c4da8b054b90b8bcfbcd319daac
minhtuan2103 <90124988+minhtuan2103@users.noreply.github.com>
2021-09-13 12:15:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,13 @@
 const CONFIG = {
     introTitle: 'TRÂM à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    introDesc: `Gửi cho cô bé Vũng Tàu đáng eooo dịp trung thu :3`,
+    btnIntro: 'U là trời =))',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Tại ông đáng iu quáa <33',
     btnNo: 'Ông bị làm sao ýyyyy :3',
     question:'Trung thu này Trâm muốn có quà gì nè <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Gửi cho tui nè <3',
     reply:'Không cần gì đâu chỉ cần có ông thoi <3',
     mess: 'Ỏooo cute z. Người gì đâu mà dễ thương thế ko biết 😘😘',
     messDesc: 'Nhắn tin cho tui nhá cô bé đáng iuuuu.',
